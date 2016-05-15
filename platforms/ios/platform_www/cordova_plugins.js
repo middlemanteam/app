7aacc48fc13a1ec4bb583e5f978349ebf9f41ca7
Nick Wanninger <nickwanninger@gmail.com>
2016-05-15 15:48:23 -0400

    tab change

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,22 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/card.io.cordova.mobilesdk/www/cdv-plugin-card-io.js",
-        "id": "card.io.cordova.mobilesdk.CardIO",
-        "pluginId": "card.io.cordova.mobilesdk",
-        "clobbers": [
-            "CardIO"
-        ]
-    },
-    {
-        "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
-        "id": "com.paypal.cordova.mobilesdk.PayPalMobile",
-        "pluginId": "com.paypal.cordova.mobilesdk",
-        "clobbers": [
-            "PayPalMobile"
-        ]
-    },
-    {
         "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
         "pluginId": "com.phonegap.plugins.PushPlugin",
@@ -36,11 +20,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "card.io.cordova.mobilesdk": "2.0.1",
-    "com.googlemaps.ios": "1.13.0",
-    "com.paypal.cordova.mobilesdk": "3.2.0",
     "com.phonegap.plugins.PushPlugin": "2.5.0",
     "cordova-plugin-whitelist": "1.2.3-dev",
+    "com.googlemaps.ios": "1.13.0",
     "plugin.google.maps": "1.3.9"
 }
 // BOTTOM OF METADATA
